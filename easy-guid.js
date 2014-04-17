37701be89a6cf91556e9b5dc12cdd2f787e902bf
Huei <huei90@gmail.com>
2014-04-17 14:23:17 +0800

    Fix error

--- easy-guid.js
+++ easy-guid.js
@@ -8,6 +8,18 @@
 
 'use strict';
 
+function S4() {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+}
+
+function guid32() {
+    return (S4()+S4()+S4()+S4()+S4()+S4()+S4()+S4());
+}
+
+function guid16() {
+    return (S4()+S4()+S4()+S4());
+}
+
 exports.new = function (type) {
 
     var value;
@@ -25,14 +37,3 @@ exports.new = function (type) {
     return value;
 };
 
-function S4() {
-    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-}
-
-function guid32() {
-    return (S4()+S4()+S4()+S4()+S4()+S4()+S4()+S4());
-}
-
-function guid16() {
-    return (S4()+S4()+S4()+S4());
-}
